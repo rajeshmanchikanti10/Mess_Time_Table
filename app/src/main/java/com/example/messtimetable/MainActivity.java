0b8d33fa4236ededa366c69d2501c1109640168c
rajeshmanchikanti10 <45352809+rajeshmanchikanti10@users.noreply.github.com>
2020-03-13 16:27:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/messtimetable/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.messtimetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+ConstraintLayout mylayout;
+AnimationDrawable animationDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mylayout=findViewById(R.id.mylayout);
+
+        /*animationDrawable=(AnimationDrawable)mylayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();*/
+    }
+}
